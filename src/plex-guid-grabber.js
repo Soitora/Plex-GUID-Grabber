002fb7abb18dbb5b96e6bac07c9647e796c78611
Soitora <simon.mattila@protonmail.com>
2025-01-29 21:36:48 +0100

    Lint project

--- src/plex-guid-grabber.js
+++ src/plex-guid-grabber.js
@@ -86,7 +86,7 @@ function handleButtons(metadata, pageType, guid) {
                     marginRight: "8px",
                     display: guid[site] ? "block" : "none",
                     opacity: 0,
-                    transition: "opacity 0.3s ease-in-out"
+                    transition: "opacity 0.3s ease-in-out",
                 },
                 html: `
                     <div class="_1h4p3k00 _1v25wbq8 _1v25wbq1w _1v25wbq1g _1v25wbq1c _1v25wbq14 _1v25wbq3g _1v25wbq2g">
